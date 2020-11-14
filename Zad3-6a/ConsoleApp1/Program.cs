f531b67c94f3ca61e05d7254f562951c8b2cce8b
KPWysocki <59768608+KPWysocki@users.noreply.github.com>
2020-11-14 13:11:13 +0100

    Add files via upload

---
+++ Zad3-6a/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoja mase w kg");
+            int masa = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj swoj wzrost w metrach:");
+            double wzrost = double.Parse(Console.ReadLine());
+            double BMI = masa / (wzrost * wzrost);
+            if (BMI < 18.5)
+            { Console.WriteLine("Masz niedowage - BMI {0}", BMI); }
+            else if (BMI >= 18.5 && BMI <= 25)
+            { Console.WriteLine("Masz BMI w normie - {0}", BMI); }
+            else if (BMI > 25)
+            { Console.WriteLine("Masz nadwage - BMI {0}", BMI); }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zad3-6b/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAPP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoja mase w kg");
+            int masa = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj swoj wzrost w metrach:");
+            double wzrost = double.Parse(Console.ReadLine());
+            double BMI = masa / (wzrost * wzrost);
+            if (BMI < 16)
+            {
+                Console.WriteLine("Jestes wyglodzony - BMI {0}", BMI);
+            }
+            else if (BMI >= 16 && BMI < 17)
+            { 
+                Console.WriteLine("Jestes wychudzony - BMI {0}", BMI); 
+            
+            }
+            else if (BMI >= 17 && BMI < 18.5)
+            {
+                Console.WriteLine("Masz niedowage - BMI {0}", BMI);
+            }
+            else if (BMI >= 18.5 && BMI < 25)
+            {
+                Console.WriteLine("Masz porzadana mase ciala - BMI {0}", BMI);
+            }
+            else if (BMI >= 25 && BMI < 30)
+            {
+                Console.WriteLine("Masz nadwage - BMI {0}", BMI);
+            }
+            else if (BMI >= 30 && BMI < 35)
+            {
+                Console.WriteLine("Masz otylosc I stopnia - BMI {0}", BMI);
+            }
+            else if (BMI >= 35 && BMI < 40)
+            {
+                Console.WriteLine("Masz otylosc II stopnia - BMI {0}", BMI);
+            }
+            else if (BMI >= 40)
+            {
+                Console.WriteLine("Masz otylosc III stopnia - BMI {0}", BMI);
+            }
+            Console.ReadKey();
+        }
+    }
+}
