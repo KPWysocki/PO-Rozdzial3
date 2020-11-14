0e58497f52a11a9836d5213e36afc6adea009f8e
KPWysocki <59768608+KPWysocki@users.noreply.github.com>
2020-11-14 13:15:24 +0100

    Add files via upload

---
+++ Zad3-8/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoja srednia:");
+            double srednia = double.Parse(Console.ReadLine());
+
+            if (srednia>=2 && srednia<=3.99)
+            { 
+                Console.WriteLine("Nie przysluguje Ci stypendium");
+            }
+            else if (srednia>=4 && srednia <= 4.79)
+            {
+                Console.WriteLine("Przysluguje Ci stypendium w wysokości 350 zł");
+            }
+            else if (srednia>4.79 && srednia <=5)
+            {
+                Console.WriteLine("Przysluguje Ci stypendium w wysokosci 550 zł");
+            }
+            Console.ReadKey();
+        }
+    }
+}
